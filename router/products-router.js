ca081c4ba0a8b4679019157e864c0513d45db091
Leandro Romero <romerodisind@gmail.com>
2023-07-13 22:27:07 -0300

    retoques

--- router/products-router.js
+++ router/products-router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
-const { io } = require('../server-express');
 
 const productRouter = express.Router();
 
