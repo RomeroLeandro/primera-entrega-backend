a17959734cbc8dd043a609173b8c79686148e5e2
Leandro Romero <romerodisind@gmail.com>
2023-08-14 22:35:06 -0300

    filter stock, category - order sort

--- router/productViewRouter.js
+++ router/productViewRouter.js
@@ -12,18 +12,50 @@ const productModel = require('../dao/model/products-model');
 productsViewsRouter.get('/', async(req, res) => {
   const page = req.query.page || 1;
   const limit = req.query.limit || 10;
+  const sort = req.query.sort || '';
+  // console.log('Page:', page); 
+  // console.log('Limit:', limit);
 
+  const category = req.query.category || '';
+  const stock = req.query.stock || '';
+
+  // console.log('Category:', category);
+
+  const query = {};
+
+  if (category) {
+    query.category = { $regex: category, $options: 'i' };
+  }
+
+  if(stock != '') {
+    query.stock = parseInt(stock);
+  }
+  
   const options = {
     page: page,
     limit: limit,
   };
 
-  const products = await productModel.paginate({ }, options);
-  console.log(products)
+  let sortQuery = {}
 
-  products.docs = products.docs.map(product => product.toObject())
+  if(sort === 'asc'){
+    sortQuery = {price:1}
+  }
+  else if(sort === 'desc'){
+    sortQuery = {price:-1}
+  }
+  const products = await productModel.paginate(query, {...options,sort: sortQuery});
+  console.log('Products:', products);
+  products.docs = products.docs.map(product => product.toObject());
 
-return res.render('products', products)
+  return res.render('products', products );
+
+//   const products = await productModel.paginate({ }, options);
+//   console.log(products)
+
+//   products.docs = products.docs.map(product => product.toObject())
+
+// return res.render('products', products)
 })
 
 module.exports = productsViewsRouter
