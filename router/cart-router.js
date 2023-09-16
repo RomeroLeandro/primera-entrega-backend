fc5e7ffaffcc062522552977a4cfd0a0cdc840fb
Leandro Romero <romerodisind@gmail.com>
2023-09-16 15:24:33 -0300

    add services, controllers and routes in cart

--- router/cart-router.js
+++ router/cart-router.js
@@ -1,75 +1,13 @@
 const { Router } = require('express');
-const CartManagerMongo = require('../dao/manager/cartManagerMongo');
-const CartManagerFile = require('../dao/manager/cartManagerFile');
+const cartController = require('../controllers/cartController');
 const cartRouter = Router();
 
-const USE_MONGO_DB = require('../config/config');
-const cartManager = USE_MONGO_DB ? new CartManagerMongo() : new CartManagerFile();
-
-cartRouter.get('/', async (req, res) => {
-  const carts = await cartManager.getCarts();
-  res.json(carts);
-});
-
-cartRouter.get('/:cid', async (req, res) => {
-  const cid = req.params.cid;
-  const cart = await cartManager.getCartById(cid);
-  return res.json(cart);
-});
-
-cartRouter.post('/', async (req, res) => {
-  const newCart = await cartManager.addCart();
-  return res.status(201).json(newCart);
-});
-
-cartRouter.post('/:cid/products/:pid', async (req, res) => {
-  const cid = req.params.cid;
-  const pid = req.params.pid;
-  try {
-    await cartManager.addProductToCart(cid, pid);
-    return res.redirect('/cart/64dab5d790a3c289d395ae2e');
-  } catch (error) {
-    
-    return console.log(error);
-  }
-});
-
-cartRouter.delete('/:cid/products/:pid', async (req, res) => {
-  const cid = req.params.cid;
-  const pid = req.params.pid;
-  
-  try {
-    await cartManager.removeProductFromCart(cid, pid);
-    return res.status(204).send();
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Error al eliminar el producto del carrito' });
-  }
-});
-
-cartRouter.put('/:cid', async (req, res) => {
-  const cartId = req.params.cid;
-  const updatedProducts = req.body.products; 
-
-  try {
-    await cartManager.updateCart(cartId, updatedProducts);
-    return res.status(200).json({ status: 'success', message: 'Carrito actualizado con éxito' });
-  } catch (error) {
-    return res.status(500).json({ status: 'error', message: 'Error al actualizar el carrito' });
-  }
-});
-
-cartRouter.put('/:cid/products/:pid', async (req, res) => {
-  const cartId = req.params.cid;
-  const productId = req.params.pid;
-  const newQuantity = req.body.quantity;
-
-  try {
-    await cartManager.updateProductQuantity(cartId, productId, newQuantity);
-    return res.status(200).json({ status: 'success', message: 'Cantidad de ejemplares actualizada con éxito' });
-  } catch (error) {
-    return res.status(500).json({ status: 'error', message: 'Error al actualizar la cantidad de ejemplares' });
-  }
-});
+cartRouter.get('/', cartController.getCart);
+cartRouter.get('/:cid', cartController.getCartById);
+cartRouter.post('/', cartController.addCart);
+cartRouter.post('/:cid/products/:pid', cartController.addProductToCart);
+cartRouter.delete('/:cid/products/:pid', cartController.removeProductFromCart);
+cartRouter.put('/:cid', cartController.updateCart);
+cartRouter.put('/:cid/products/:pid', cartController.updateProductQuantity);
 
 module.exports = cartRouter;
