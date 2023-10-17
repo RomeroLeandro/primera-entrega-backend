07c3c91448d3cf12cdd3353270a9aa5f20b27f03
Leandro Romero <romerodisind@gmail.com>
2023-10-17 19:53:17 -0300

    logger

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,67 +1,68 @@
 const cartService = require('../services/cartServices');
-const errorMessages = require('../errors/errorController');
+const customErrorHandler = require('../errors/errorController');
+const logger = require('../logger');
 
 async function getCart(req, res) {
     try {
-        const carts = await cartService.getCart();
-        res.json(carts);
+      const carts = await cartService.getCart();
+      res.json(carts);
     } catch (error) {
-        console.error(error);
-        const errorMessage = customErrorHandler('CART_FETCH_FAILED');
-        res.status(500).json(errorMessage);
+      logger.error(`Error en getCart: ${error}`);
+      const errorMessage = customErrorHandler('CART_FETCH_FAILED');
+      res.status(500).json(errorMessage);
     }
-}
-
-async function getCartById(req, res) {
+  }
+  
+  async function getCartById(req, res) {
     const cid = req.params.id;
     try {
-        const cart = await cartService.getCartById(cid);
-        res.json(cart);
+      const cart = await cartService.getCartById(cid);
+      res.json(cart);
     } catch (error) {
-        console.error(error);
-        const errorMessage = customErrorHandler('CART_FETCH_FAILED');
-        res.status(500).json(errorMessage);
+      logger.error(`Error en getCartById: ${error}`);
+      const errorMessage = customErrorHandler('CART_FETCH_FAILED');
+      res.status(500).json(errorMessage);
     }
-}
-
-async function addCart(req, res) {
+  }
+  
+  async function addCart(req, res) {
     try {
-        const cart = await cartService.addCart(req.body);
-        res.status(201).json(newCart);
+      const cart = await cartService.addCart(req.body);
+      res.status(201).json(newCart);
     } catch (error) {
-        console.error(error);
-        const errorMessage = customErrorHandler('CART_ADD_FAILED');
-        res.status(500).json(errorMessage);
+      logger.error(`Error en addCart: ${error}`);
+      const errorMessage = customErrorHandler('CART_ADD_FAILED');
+      res.status(500).json(errorMessage);
     }
-}   
-
-async function addProductToCart(req, res) {
-    const cid = req.params.id;
-    const pid = req.params.id;
-    try {
-        const cart = await cartService.addProductToCart(cid, pid);
-        res.status(201).json(cart);
-    } catch (error) {
-        console.error(error);
-        const errorMessage = customErrorHandler('PRODUCT_ADD_TO_CART_FAILED');
-        res.status(500).json(errorMessage);
-    }
-}
-
-async function removeProductFromCart(req, res) {
+  }
+  
+  async function addProductToCart(req, res) {
     const cid = req.params.id;
     const pid = req.params.id;
     try {
-        await cartService.removeProductFromCart(cid, pid);
-        res.status(204).json({ status: 'success', message: 'Producto eliminado del carrito con éxito' });
+      const cart = await cartService.addProductToCart(cid, pid);
+      res.status(201).json(cart);
     } catch (error) {
-        console.error(error);
-        const errorMessage = customErrorHandler('PRODUCT_REMOVE_FROM_CART_FAILED');
-        res.status(500).json(errorMessage);
+      logger.error(`Error en addProductToCart: ${error}`);
+      const errorMessage = customErrorHandler('PRODUCT_ADD_TO_CART_FAILED');
+      res.status(500).json(errorMessage);
     }
-}
-
-module.exports = {
+  }
+  
+  async function removeProductFromCart(req, res) {
+    const cid = req.params.id;
+    const pid = req.params.id;
+    try {
+      await cartService.removeProductFromCart(cid, pid);
+      res.status(204).json({ status: 'success', message: 'Producto eliminado del carrito con éxito' });
+    } catch (error) {
+      logger.error(`Error en removeProductFromCart: ${error}`);
+      const errorMessage = customErrorHandler('PRODUCT_REMOVE_FROM_CART_FAILED');
+      res.status(500).json(errorMessage);
+    }
+  }
+  
+  module.exports = {
     getCart,
     getCartById,
     addCart,
@@ -70,4 +71,4 @@ module.exports = {
     updateCart,
     updateProductQuantity,
     purchaseCart
-}
+  };
--- logger.js
+++ logger.js
@@ -1,24 +1,26 @@
 const winston = require('winston');
 
 function createLogger() {
-    if (process.env.NODE_ENV === 'production') {
-      // Configuración del logger de producción
-      return winston.createLogger({
-        levels: winston.config.npm.levels,
-        format: winston.format.json(),
-        transports: [
-          new winston.transports.File({ filename: 'error.log', level: 'error' }),
-          new winston.transports.File({ filename: 'combined.log' }),
-        ],
-      });
-    } else {
-      // Configuración del logger de desarrollo
-      return winston.createLogger({
-        levels: winston.config.npm.levels,
-        format: winston.format.simple(),
-        transports: [new winston.transports.Console({ level: 'debug' })],
-      });
-    }
+  if (process.env.NODE_ENV === 'production') {
+    // Configuración del logger de producción
+    return winston.createLogger({
+      levels: winston.config.npm.levels,
+      format: winston.format.json(),
+      transports: [
+        new winston.transports.File({ filename: 'errors.log', level: 'error' }),
+      ],
+    });
+  } else {
+    // Configuración del logger de desarrollo
+    return winston.createLogger({
+      levels: winston.config.npm.levels,
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+      transports: [new winston.transports.Console({ level: 'debug' })],
+    });
   }
-  
-  module.exports = createLogger();
+}
+
+module.exports = createLogger();
